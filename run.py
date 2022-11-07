bd6eb24d5aded65c4f61b5150db886ab7c316085
Naka Freitag <spamnaka@gmail.com>
2022-11-07 13:20:15 -0500

    Start server before blanca

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import subprocess
 from ShadyMarket import app
 from ShadyMarket import blanca
 
-subprocess.call(blanca)
+
 port = int(os.environ.get('PORT', 33507))
 app.run(host='0.0.0.0', port=port)
+subprocess.call(blanca)
