af9c796b3346eab48e0de1c756e4bf694157053c
Naka Freitag <spamnaka@gmail.com>
2022-11-07 13:04:23 -0500

    attempting to fix pathing to app file for discord

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
+import sys
 from ShadyMarket import app
 
-exec("./ShadyMarket/app.py")
-
+exec(os.path.join(os.path.dirname(sys.argv[0]), 'ShadyMarket', 'app.py'))
 port = int(os.environ.get('PORT', 33507))
 app.run(host='0.0.0.0', port=port)
