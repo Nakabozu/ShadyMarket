3847426648937cb808c7b8f3a060ed000ac86165
Naka Freitag <spamnaka@gmail.com>
2022-11-07 13:17:18 -0500

    Finally fixed blanca

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import os
-import sys
-from ShadyMarket import app
+import subprocess
 
-exec(os.path.join(os.path.dirname(sys.argv[0]), 'ShadyMarket', 'app.py'))
+from ShadyMarket import app
+from ShadyMarket import blanca
+
+subprocess.call(blanca)
 port = int(os.environ.get('PORT', 33507))
 app.run(host='0.0.0.0', port=port)
