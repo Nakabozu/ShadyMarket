016ce9d1b10f115ec3bd5f849d54796c104f084d
Naka Freitag <spamnaka@gmail.com>
2022-11-07 14:35:12 -0500

    Fixing port issues

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from ShadyMarket import blanca
 
 port = int(os.environ.get('PORT', 33507))
 print("Creating Flask Process")
-flaskProcess = Thread(target=lambda: app.run(host="0.0.0.0", port=23336, debug=False, use_reloader=False))
+flaskProcess = Thread(target=lambda: app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False))
 print("Creating Blanca Process")
 blancaProcess = Thread(target=blanca.open_shop, name="blancaThread")
 
